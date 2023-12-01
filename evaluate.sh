c064b9d889188881a54c21e32272bf945ac624a8
friedrich.hastedt@gmail.com <you@example.com>
2023-12-01 17:43:36 +0000

    env changes

--- evaluate.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-conda activate evalretro
-export DATAPATH=./data
-export CONFIGPATH=./config
-
-python main.py --k_back 10 --k_forward 2 --invsmiles 20 --fwd_model 'gcn' --config_name 'raw_data.json' --quick_eval True 
